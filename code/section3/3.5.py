74618fcbd65e9aed9bfdafd5e075de144aeec41a
嘉军 <15302185+wjj0123@user.noreply.gitee.com>
2026-01-19 12:02:31 +0800

    1st commit

--- code/section3/3.5.py
+++ code/section3/3.5.py
@@ -12,7 +12,7 @@ A,B,C,D,E,F,x,y = sp.symbols('A,B,C,D,E,F,x,y')
 expr = A*x**2 + B * x * y + C * y**2 +D * x + E * y + F
 eq = sp.Eq(expr,0)
 # 如果圆锥曲线在原点,则没有x和y的一次项,也就是D和E为0
-subs_dict = {A:-3,B:0,C:-4,D:0,E:0,F:4}
+subs_dict = {A:-3,B:4,C:-4,D:0,E:0,F:4}
 result = eq.subs(subs_dict)
 
 # 解出y关于x的表达式
@@ -37,14 +37,21 @@ for i, sol in enumerate(solutions):
         print(f"绘制解 {i+1} 时出错: {e}")
         continue
 
-ax.text(0.,0.,'$-3x^2 + 3xy - 4y^2 + 5x + 8y + 4 = 0$',
+ax.text(0.,0.,f'${sp.latex(result)}$',
         bbox={'boxstyle': 'round', 
               'facecolor': 'lightblue',
               'alpha': 0.7,
               'pad': 0.5}
-        ,alpha = 0.4)
-ax.text(0.,1,f'$y={sp.latex(solutions[0])}$',fontsize = 12)
-ax.text(0.,0.8,f'$y={sp.latex(solutions[1])}$',fontsize = 12)
+        ,alpha = 0.8)
+
+offset_x = 0.5  # x方向的偏移量（向左为负）
+offset_y = 0.5  # y方向的偏移量（向下为负）
+text_x = ax.get_xlim()[0] + offset_x  # 获取x轴最小值并加上偏移量
+text_y = ax.get_ylim()[0] + offset_y  # 获取y轴最小值并加上偏移量
+
+ax.text(text_x,text_y,f'$y={sp.latex(solutions[0])}$',fontsize = 12)
+ax.text(text_x,text_y + offset_y,f'$y={sp.latex(solutions[1])}$',fontsize = 12)
+
 # 文件保存
 script_name = os.path.splitext(os.path.basename(__file__))[0]
 plt.savefig("figures/section3/" + script_name + '.png')
---
+++ code/section3/3.6.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import sympy as sp
+import numpy as np
+import os
+
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+fig,ax = plt.subplots()
+
+x,y = sp.symbols("x y")
+expr1 = x + y + 1
+expr2 = x**2  +  y**2 + 3*x + 4*y 
+
+eq1,eq2 = sp.Eq(expr1,0),sp.Eq(expr2,0)
+results = sp.solve([eq1,eq2],[x,y])
+
+y1 = sp.solve(eq1,y)
+y2 = sp.solve(eq2,y)
+
+# 绘制每个解
+x_vals = np.linspace(-5, 5, 2000)
+for i, sol in enumerate(y1):
+    y_func = sp.lambdify(x, sol, 'numpy')
+    y_vals = y_func(x_vals)
+    ax.plot(x_vals, y_vals,color = 'red')
+    
+
+for i, sol in enumerate(y2):
+    y_func = sp.lambdify(x, sol, 'numpy')
+    y_vals = y_func(x_vals)
+    ax.plot(x_vals, y_vals,color = 'red')
+    
+
+# 寻找交点
+for result in results:
+    plt.plot(*result,'ro')
+
+
+plt.show()
